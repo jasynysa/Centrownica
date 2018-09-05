7b9a48d1ca31c5171549bc5a6d8abe20ad56ffd2
Patryk Jaseniuk <jasynysa@gmail.com>
2018-09-05 14:59:01 +0200

    Klasa Obrecz posiada fizyczne wlasciwosci

--- ConsoleApplication1/Obrecz.h
+++ ConsoleApplication1/Obrecz.h
@@ -1,16 +1,33 @@
 #pragma once
+#include"glm/glm.hpp"
 class Obrecz
 {
 public:
 	Obrecz(int iloscSegmentow, float groboscObreczy);
 	~Obrecz();
 
+
+	///model grafiki
 	float groboscObreczy;//jako czesc promienia obreczy
 	float* tablicaWierzcholkow;
 	int* tablicaElementow;
 	int iloscWierzcholkow;
 	int iloscElementow;
+	int iloscSegmentow;
 	int rozmiarTablicyWierzcholkow;
 	int rozmiarTablicyElementow;
+
+
+	///model fizyki
+	void regulujSzpryche(int ktora, float dlugosc);
+
+	glm::fvec3* tablicaSil;//tablica w ktorej przechowywane sa coœ jakby si³y dzia³aj¹ce na poszczegolne punkty, jednak nie zmieniaja one predkosci punktow, tylko ich po³o¿enie, wiêc bardziej przypomina to prêdkoœæ
+	//glm::fvec3 tablicaSil[200];
+	int** tablicaOddzialywan;
+	float* tablicaDlugosciSzprych;
+
+	float silaMax;
+	float przesuniecieMax;
+
 };
 
